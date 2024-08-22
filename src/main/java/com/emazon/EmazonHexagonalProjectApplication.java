66d2b78cde4b5f3c1c784d994bd0c1afa7ee0f7e
Danielalvz <danivasquez0310@gmail.com>
2024-08-22 16:56:03 -0500

    Init project

---
+++ src/main/java/com/emazon/EmazonHexagonalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.emazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonHexagonalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonHexagonalProjectApplication.class, args);
+	}
+
+}
